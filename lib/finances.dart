0dbc789ba1784a9a2ebc76535bc1d1bec3df19ae
Malcolm <buildingwithkieth@outlook.com>
2025-01-11 23:22:01 -0600

    Added health and finances pages

---
+++ lib/finances.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FinancesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Finances')),
+      body: Center(
+        child: Text('Welcome to the Finances Page!'),
+      ),
+    );
+  }
+}
---
+++ lib/health.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HealthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Health')),
+      body: Center(
+        child: Text('Welcome to the Health Page!'),
+      ),
+    );
+  }
+}
